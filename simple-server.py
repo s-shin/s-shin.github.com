88bbc7b6d3dfe1b3af2dd6059fbc0edefca9bf71
shin <s2pch.luck@gmail.com>
2013-01-24 22:30:24 +0900

    update Twitter Bootstrap and using CDN modify some style

--- simple-server.py
+++ simple-server.py
@@ -1,7 +1,9 @@
 #! /usr/bin/env python3
 
+import sys
 import http.server
 
+port = 3000 if len(sys.argv) == 1 else int(sys.argv[1])
 httpd = http.server.HTTPServer(
-	('localhost', 3000), http.server.SimpleHTTPRequestHandler)
+  ('localhost', port), http.server.SimpleHTTPRequestHandler)
 httpd.serve_forever()
